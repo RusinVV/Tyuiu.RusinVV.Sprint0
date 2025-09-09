223bd63a8863db96034dd6e6a4e004f01fdfe245
Computer <Computer@DESKTOP-SVJAOV0>
2025-09-09 14:00:12 +0500

    Task1

--- Tyuiu.RusinVV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.RusinVV.Sprint0.Task1.V0/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.RusinVV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Владислав";
+            int age = 19;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
